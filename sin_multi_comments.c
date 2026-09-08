77fc2136f3104f74a0b418b5281fe12acd84159c
priyanshu813109-stack <priyanshu813109@gmail.com>
2026-09-08 14:56:11 +0530

    Add files via upload

---
+++ sin_multi_comments.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+// this is a single line comment.
+printf("hello, world\n");
+/*
+this is a multi-line comment.
+it can contain multiple line.
+*/
+printf("This program demonstrates comments");
+return 0;
+}
+    
